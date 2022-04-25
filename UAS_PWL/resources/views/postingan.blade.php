c7f5b8a62a957cd9ccb4ccbc98cde426986bc7fa
Dodi Devrian Andrianto <84231458+DodiDevrian@users.noreply.github.com>
2022-04-25 10:34:32 +0700

    Update postingan.blade.php
    Menambah class pada element untuk styling

--- UAS_PWL/resources/views/postingan.blade.php
+++ UAS_PWL/resources/views/postingan.blade.php
@@ -4,38 +4,43 @@
 @extends('layouts.app')
 
 @section('content')
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-        <div class="container">
+    <div class="container-fluid">
+        <nav class="navbar navbar-expand-sm navbar-light navbar-literasi bg-white shadow-sm">
             <a class="navbar-brand" href="{{ url('/') }}">
-                LiterasiKuy
+                {{-- {{ config('app.name', 'Laravel') }} SDFSDF --}}
+                <img src="/assets/images/logo.svg" width="185px" height="50px">
             </a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav me-auto">
-                    <a href="/kategori">Kategori</a>
-                </ul>
-                <ul class="navbar-nav me-auto">
-                    <a href="/bantuan">Bantuan</a>
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ms-auto">
+                <ul class="navbar-nav nav-menu ml-auto">
+                    <li class="menu-list">
+                        <a class="menu-link" href="/kategori">Kategori</a>
+                    </li>
+                    <li class="menu-list">
+                        <a class="menu-link" href="/bantuan">Bantuan</a>
+                    </li>
+                    <!-- Right Side Of Navbar -->
                     <!-- Authentication Links -->
                     @guest
                         @if (Route::has('login'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            <li class="nav-item menu-list">
+                                <a class="btn-lgn" href="{{ route('login') }}">{{ __('Sign In') }}</a>
                             </li>
                         @endif
 
                         @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            <li class="nav-item menu-list">
+                                <a class="btn-rgt" href="{{ route('register') }}">{{ __('Sign Up') }}</a>
                             </li>
                         @endif
                     @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                        <li class="nav-item dropdown menu-list">
+                            <a id="navbarDropdown" class="dropdown-toggle btn-lgn" href="#" role="button"
                                 data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->name }}
                             </a>
@@ -43,7 +48,7 @@
                             <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                     onclick="event.preventDefault();
-                                                                                                                                                                                                                                                                                                             document.getElementById('logout-form').submit();">
+                                                                                                                                                                                                                                                                             document.getElementById('logout-form').submit();">
                                     {{ __('Logout') }}
                                 </a>
 
@@ -55,53 +60,48 @@
                     @endguest
                 </ul>
             </div>
-        </div>
-    </nav>
+        </nav>
+    </div>
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-12">
-                <div class="row justify-content-center">
+        <div class="row">
+            <div class="col-md-12 col-lg-12">
+                <div class="row action-page">
                     @guest
                     @else
-                        <div class="col-md-4">
-                            <a class="btn btn-warning" href="/tambah" role="button"> + Posting Karya</a>
+                        <div class="col-12 col-md-4">
+                            <a class="btn btn-warning btn-post" href="/tambah" role="button"> + Posting Karya</a>
                         </div>
                     @endguest
-                    <div class="col-md-2">
-                    </div>
-                    <div class="col-md-6">
-                        <form action="/cari" method="GET">
-                            <div class="input-group">
-                                <input type="text" name="cari" id="form1" placeholder="Masukkan kata kunci .."
-                                    class="form-control" value="{{ old('cari') }}">
-                                <input class="btn btn-primary ml-2" type="submit" value="CARI">
-                            </div>
-                        </form>
-                    </div>
+                    
+                        <div class="col-12 col-md-6 ml-auto">
+                            <form action="/cari2" method="GET">
+                                <div class="input-group">
+                                    <input type="text" name="cari" id="form1" placeholder="Masukkan kata kunci .."
+                                        class="form-control" value="{{ old('cari') }}">
+                                    <input class="btn btn-primary ml-2" type="submit" value="CARI">
+                                </div>
+                            </form>
+                        </div>
+
                 </div>
 
 
                 @foreach ($literasi as $p)
                     <div class="card" style="background-color: #E8F4FF">
-                        <div class="row justify-content-center">
-                            <div class="col-md-3">
-                                <img src="/storage/thumbnail/{{ $p->image }}" class="card-img-top" alt="...">
+                        <div class="text-center">
+                            <h4 class="card-title sub-head">{{ $p->judul }}</h4>
+                            <hr width="30%" margin="auto" color="#1F76C6" style="opacity: 20%;">
+                        </div>
+                        <div class="post-thumbnail">
+                            <img src="/storage/thumbnail/{{ $p->image }}" width="100%" style="border-radius: 10px;" alt="...">
+                            <div class="info">
+                                <p>Karya: {{ $p->name }} | Diposting: {{ $p->waktu_post }} | Kategori: {{ $p->kategori }}</p>
                             </div>
-
+                            
+                        </div>
+                        <div class="row justify-content-center">
                             <div class="card-body col-md-8">
-                                <h4 class="card-title">{{ $p->judul }}</h4>
-                                <h6 class="card-category">{{ $p->kategori }}</h6>
-                                <p style="text-align:justify" class="card-text">{{ $p->isi }}
-                                </p>
-
-                                <div class="row">
-                                    <div class="col-md-8">
-                                        <p>Oleh: {{ $p->name }}, {{ $p->waktu_post }}</p>
-                                    </div>
-
-
-
-                                </div>
+                                <p style="text-align:justify" class="card-text">{{ $p->isi }}</p>
                             </div>
                         </div>
                     </div>
