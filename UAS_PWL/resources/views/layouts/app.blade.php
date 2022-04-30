556208c6144884612c0120de7dbbf36ab0f35ca7
119140037 - Joy Ravelo Tarigan <56099222+joyrav09@users.noreply.github.com>
2022-04-30 18:20:38 +0700

    Add files via upload

--- UAS_PWL/resources/views/layouts/app.blade.php
+++ UAS_PWL/resources/views/layouts/app.blade.php
@@ -22,8 +22,10 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,700;1,400&display=swap" rel="stylesheet">
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
 </head>
 
@@ -66,7 +68,6 @@
             <!-- Copyright -->
         </footer>
     </footer>
-
 </body>
 
 </html>
