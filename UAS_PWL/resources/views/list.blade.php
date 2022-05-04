5b7d78f37a55265dcba95be6ac54a174c69920c2
Frinaldo Sinaga <84637791+Sinagafrinaldo@users.noreply.github.com>
2022-05-04 19:47:35 +0700

    Update list.blade.php

--- UAS_PWL/resources/views/list.blade.php
+++ UAS_PWL/resources/views/list.blade.php
@@ -1,89 +1,126 @@
-<!DOCTYPE html>
-<html lang="en">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
+@extends('layouts.app')
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+@section('content')
+    <nav class="navbar navbar-expand-sm navbar-light navbar-literasi bg-white shadow-sm">
+        <a class="navbar-brand" href="{{ url('/') }}">
+            {{-- {{ config('app.name', 'Laravel') }} SDFSDF --}}
+            <img src="assets/images/logo.svg" width="185px" height="40px">
+        </a>
 
-    <title>Dashboard</title>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-    <!-- Favicon-->
-    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+            <ul class="navbar-nav nav-menu ml-auto">
+                <li class="menu-list">
+                    <a class="menu-link" href="/kategori">Kategori</a>
+                </li>
+                <li class="menu-list">
+                    <a class="menu-link" href="/bantuan">Bantuan</a>
+                </li>
+                <!-- Right Side Of Navbar -->
+                <!-- Authentication Links -->
+                @guest
+                    @if (Route::has('login'))
+                        <li class="nav-item menu-list">
+                            <a class="btn-lgn" href="{{ route('login') }}">{{ __('Sign In') }}</a>
+                        </li>
+                    @endif
 
-    <!-- Core theme CSS (includes Bootstrap)-->
-    <link href={{ asset('vendor/css/styles.css') }} rel="stylesheet" />
+                    @if (Route::has('register'))
+                        <li class="nav-item menu-list">
+                            <a class="btn-rgt" href="{{ route('register') }}">{{ __('Sign Up') }}</a>
+                        </li>
+                    @endif
+                @else
+                    <li class="nav-item dropdown menu-list">
+                        <a id="navbarDropdown" class="dropdown-toggle btn-lgn" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}
+                        </a>
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
-</head>
+                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                                                                                                                                                                                                                                                                                                            document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
 
-<body style="background-color:#F6F6F6">
-    <div class="d-flex" id="wrapper">
-        <!-- Sidebar-->
-        <div class="border-end " id="sidebar-wrapper" style="background-color: #1c70bd;">
-            <div class="sidebar-heading border-bottom text-white"><strong>LiterasiKuy</strong></div>
-            <div class="list-group list-group-flush">
-                <a class="list-group-item list-group-item-action text-white  p-3" href="
-                    {{ route('admin.home') }}" style="background-color: #1c70bd">
-                    Data Post
-                </a>
-                <a class="list-group-item list-group-item-action text-white p-3" href="{{ route('admin.users') }}"
-                    style="background-color: #1c70bd">
-                    User
-                </a>
-                <a class="list-group-item list-group-item-action text-white p-3"
-                    href="{{ route('admin.laporan-masalah') }}" style="background-color: #1c70bd">
-                    Laporan Masalah
-                </a>
-
-            </div>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @endguest
+            </ul>
         </div>
-        <!-- Page content wrapper-->
-        <div id="page-content-wrapper">
-            <!-- Top navigation-->
-            <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-                <div class="container-fluid">
-                    <button class="btn btn-primary" id="sidebarToggle">Toggle Menu</button>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation"><span
-                            class="navbar-toggler-icon"></span></button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
-                            <li class="nav-item active"><a class="nav-link"
-                                    href="{{ route('admin.home') }}">Home</a></li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button"
-                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    {{ Auth::guard('adminMiddle')->user()->name }}
-                                </a>
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item"
-                                        href="{{ route('admin.logout') }}">{{ _('Logout') }}</a>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+    </nav>
 
-            <!-- Page content-->
-            <div class="container-fluid" style="width:98%">
-                @yield('content')
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-lg-12">
+                <div class="row action-page" style="margin-top: -20">
+                    @guest
+                    @else
+                        <div class="col-12 col-md-4">
+                            <a class="btn btn-warning btn-post" href="/tambah" role="button"> + Posting Karya</a>
+                        </div>
+                    @endguest
+
+                    <div class="col-12 col-md-6 ml-auto">
+                        <form action="/cari2" method="GET">
+                            <div class="input-group">
+                                <input type="text" name="cari" id="form1" placeholder="Masukkan kata kunci .."
+                                    class="form-control" value="{{ old('cari') }}">
+                                <input class="btn btn-primary ml-2" type="submit" value="CARI">
+                            </div>
+                        </form>
+                    </div>
+
+                </div>
+
+                @foreach ($literasi as $p)
+                    <div class="shadow p-3 mb-5 bg-white rounded" style="margin-top: -12">
+                        <div class="card border-0">
+                            <div class="row justify-content-start">
+                                <div class="col-md-3 align-self-center">
+                                    <div class="rounded float-left">
+                                        <img src="/img/uploads/{{ $p->image }}"
+                                            class="card-img-top img-sm-thumbnail border-0" alt="...">
+                                    </div>
+                                </div>
+
+                                <div class="col-md-9 align-self-center">
+                                    <h4 class="card-title">{{ $p->judul }}</h4>
+                                    <h6 class="card-category">{{ $p->kategori }}</h6>
+                                    <p style="text-align:justify" class="card-text">{!! Str::limit($p->isi, 100) !!}
+                                    </p>
+
+                                    <div class="row">
+                                        <div class="col-md-9">
+                                            <p>Oleh: {{ $p->name }}, {{ $p->waktu_post }}</p>
+                                        </div>
+                                        <div class="col ms-auto">
+                                            <a href="/postingan/{{ $p->slug }}">Baca Selengkapnya >>></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
             </div>
+            <div class="pagin">
+                {{ $literasi->links() }}
+            </div>
+
         </div>
     </div>
-    <!-- Bootstrap core JS-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- Core theme JS-->
-    <script src="{{ asset('vendor/js/scripts.js') }}"></script>
-    <script src="js/scripts.js"></script>
-</body>
-
-</html>
+@endsection
