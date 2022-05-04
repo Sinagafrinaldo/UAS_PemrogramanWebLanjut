33138026cf2851880aa63f057ecb5534ed783824
Frinaldo Sinaga <84637791+Sinagafrinaldo@users.noreply.github.com>
2022-05-04 22:04:37 +0700

    Update postingan.blade.php

--- UAS_PWL/resources/views/dashboard/postingan.blade.php
+++ UAS_PWL/resources/views/dashboard/postingan.blade.php
@@ -1,32 +1,50 @@
 @extends('layouts._dashboard.app')
-
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-12">
-                <br>
-                @foreach ($literasi as $p)
-                    <div class="card" style="background-color: #E8F4FF">
-                        <div class="row justify-content-center">
-                            <div class="col-md-3">
-                                <img src="/img/uploads/{{ $p->image }}" class="card-img-top" alt="...">
+        <div class="row">
+            <div class="col-md-12 col-lg-12">
+                <div class="row action-page" style="margin-top: -20">
+                    @guest
+                    @else
+                        <div class="col-12 col-md-4">
+                            <a class="btn btn-warning btn-post" href="/tambah" role="button"> + Posting Karya</a>
+                        </div>
+                    @endguest
+
+                    <div class="col-12 col-md-6 ml-auto">
+                        <form action="/cari2" method="GET">
+                            <div class="input-group">
+                                <input type="text" name="cari" id="form1" placeholder="Masukkan kata kunci .."
+                                    class="form-control" value="{{ old('cari') }}">
+                                <input class="btn btn-primary ml-2" type="submit" value="CARI">
                             </div>
+                        </form>
+                    </div>
 
-                            <div class="card-body col-md-8">
-                                <h4 class="card-title">{{ $p->judul }}</h4>
-                                <h6 class="card-category">{{ $p->kategori }}</h6>
-                                <p style="text-align:justify" class="card-text">{{ $p->isi }}
-                                </p>
+                </div>
 
-                                <div class="row">
-                                    <div class="col-md-8">
-                                        <p>Oleh: {{ $p->name }}, {{ $p->waktu_post }}</p>
-                                    </div>
-                                </div>
+                @foreach ($literasi as $p)
+                    <div class="shadow p-4 mb-5 bg-white rounded" style="margin-top: -11;">
+                        <div class="text-center" style="margin-top:20;">
+                            <h3 class="card-title sub-head" style="color: #1F76C6">{{ $p->judul }}</h3>
+                            <hr width="50%" margin="auto" color="#1F76C6" style="opacity: 20%;">
+                        </div>
+                        <div class="post-thumbnail">
+                            <div>
+                                <img src="/img/uploads/{{ $p->image }}" class="img-fluid mx-auto d-block" alt="...">
                             </div>
                         </div>
+                        <div class="row justify-content-center">
+                            <div class="card-body text-justify">
+                                <p>{!! $p->isi !!}</p>
+                            </div>
+                        </div>
+
+                        <div class="info " style="font-size: 14">
+                            <p>Karya: {{ $p->name }} | Diposting: {{ $p->waktu_post }} | Kategori:
+                                {{ $p->kategori }}</p>
+                        </div>
                     </div>
-                    <br>
                 @endforeach
             </div>
         </div>
