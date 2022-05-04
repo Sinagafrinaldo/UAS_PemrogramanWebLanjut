605a357f55999c9fa1cc05377f6565ed6885667b
Frinaldo Sinaga <84637791+Sinagafrinaldo@users.noreply.github.com>
2022-05-04 22:04:51 +0700

    Update laporan-masalah.blade.php

--- UAS_PWL/resources/views/dashboard/laporan-masalah.blade.php
+++ UAS_PWL/resources/views/dashboard/laporan-masalah.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts._dashboard.app')
 
 @section('content')
-    <h1 class="mt-1">Data Postingan</h1>
+    <h1 class="mt-1">Laporan Masalah</h1>
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="col-md-2">
@@ -21,38 +21,61 @@
         @foreach ($laporan as $p)
             <div class="shadow p-3 mb-4 bg-white rounded">
                 <div class="card border-0">
-                    <div class="row justify-content-start">
-                        <div class="col-md-3 align-self-center">
+                    <div class="row justify-content">
+                        {{-- <div class="col-md-3 align-self-center">
                             @if ($p->image_laporan != null)
                                 <div class="rounded float-left">
                                     <img src="/img/upload-laporan/{{ $p->image_laporan }}"
                                         class="card-img-top img-sm-thumbnail border-0" alt="...">
                                 </div>
                             @endif
-                        </div>
-
-                        <div class="col-md-9 align-self-center">
+                        </div> --}}
+                        <div class="col-md " style="padding:30px">
                             <h4 class="card-title">{{ $p->judul_laporan }}</h4>
-                            <p style="text-align:justify" class="card-text">{!! $p->isi_laporan !!}
+                            <hr>
+                            {{-- <h6 class="card-category">{{ $p->kategori_request }}</h6> --}}
+                            <p class="text-justify">
+                                {!! $p->isi_laporan !!}
                             </p>
-                            {{-- <p>Status: {{ $p->status }}</p> --}}
-                            @if ($p->status == 'Belum Ditanggapi')
-                                <div class="btn btn-secondary">Belum Ditanggapi</div>
-                            @elseif($p->status == 'Diterima')
-                                <div class="btn btn-success ">Diterima</div>
-                            @elseif($p->status == 'Ditolak')
-                                <div class="btn btn-danger">Ditolak</div>
-                            @endif
 
+                            @if ($p->image_laporan != null)
+                                <strong>Lampiran:</strong><br><br>
+                                <div class="rounded">
+                                    <img src="/img/upload-laporan/{{ $p->image_laporan }}"
+                                        class="card-img-top img-sm-thumbnail border-0" style="width: 50%; height:50%"
+                                        alt="...">
+                                </div>
+                            @endif
+                            <hr>
                             <div class="row">
                                 <div class="col-md-9">
-                                    <p>Oleh: {{ $p->name }}, {{ $p->waktu }}</p>
+                                    <p>Oleh: {{ $p->name }}</p>
+                                    <p>Waktu: {{ $p->waktu }}</p>
                                 </div>
+
                             </div>
+                            <div class=" row justify-content-center">
+
+                                @if ($p->status == 'Belum Ditanggapi')
+                                    <div class="btn btn-secondary col-sm-2">Belum Ditanggapi</div>
+                                @elseif($p->status == 'Diterima')
+                                    <div class="btn btn-success col-sm-2 ">Diterima</div>
+                                @elseif($p->status == 'Ditolak')
+                                    <div class="btn btn-danger col-sm-2">Ditolak</div>
+                                @endif
+                                &emsp;
+                                <a class="btn btn-warning col-sm-2"
+                                    href="laporan-masalah/hapus-laporan/{{ $p->id_laporan }}">Hapus</a>
+
+
+
+                            </div>
+
                         </div>
+
+
                     </div>
-                    <a class="btn btn-warning col-md-1"
-                        href="laporan-masalah/hapus-laporan/{{ $p->id_laporan }}">Hapus</a>
+
                     <hr>
                     <div class="">
                         <form action="{{ route('admin.update-status') }}" method="post" enctype="multipart/form-data">
@@ -70,7 +93,7 @@
                                     </select>
                                 </div>
                                 <input type="text" name="id_laporan" hidden value="{{ $p->id_laporan }}">
-                                
+
                                 <div class="col-md-1">
                                     <button type="submit" value="Simpan Data" class="btn btn-primary">Submit</button>
                                 </div>
