a84ea6e4bf372e4448a6a7c63d11b96bb811bc12
Frinaldo Sinaga <84637791+Sinagafrinaldo@users.noreply.github.com>
2022-05-06 16:08:22 +0700

    Update login.blade.php

--- UAS_PWL/resources/views/dashboard/auth/login.blade.php
+++ UAS_PWL/resources/views/dashboard/auth/login.blade.php
@@ -1,13 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="csrf-token" content="{{csrf_token()}}">
-    <title>Halaman login Administrator</title>
-<link rel="stylesheet" href="{{asset('vendor/css/styles.css')}}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Login Admin || LiterasiKUY</title>
+    <link rel="stylesheet" href="{{ asset('vendor/css/styles.css') }}">
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,700;1,400&display=swap"
+        rel="stylesheet">
+    <!-- Styles -->
+    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <style>
+        body {
+            background-image: url("assets/images/bg.jpg");
+            background-repeat: no-repeat, repeat;
+            background-size: 100%;
+            font-family: 'Poppins', sans-serif;
+
+        }
+
+    </style>
 </head>
+
 <body>
     <div id="layoutAuthentication_content">
         <main>
@@ -15,31 +41,37 @@
                 <div class="row justify-content-center">
                     <div class="col-lg-5">
                         <div class="card shadow-lg border-0 rounded-lg mt-5">
-                            <div class="card-header"><h3 class="text-center font-weight-light my-4">Login Administrator</h3></div>
+                            <div class="card-header">
+                                <h3 class="text-center font-weight-light my-4">Login Administrator</h3>
+                            </div>
                             <div class="card-body">
                                 @if (Session::has('success'))
                                     <div class="alert alert-success" role="alert">
-                                        {{Session::get('success')}}
+                                        {{ Session::get('success') }}
                                     </div>
                                 @endif
-                            <form method="POST" action="{{route('admin.login')}}" novalidate>
-                                @csrf
+                                <form method="POST" action="{{ route('admin.login') }}" novalidate>
+                                    @csrf
                                     <div class="form-group">
                                         <label class="small mb-1" for="inputEmailAddress">Email</label>
-                                    <input class="form-control py-4 @error('email') is-invalid @enderror" id="inputEmailAddress" type="email" placeholder="Isi email" name="email" value="{{old('email')}}" />
-                                     @error('email')
-                                        <span class="invalid-feedback"><strong>{{$message}}</strong></span>
-                                     @enderror   
-                                </div>
+                                        <input class="form-control py-4 @error('email') is-invalid @enderror"
+                                            id="inputEmailAddress" type="email" placeholder="Isi email" name="email"
+                                            value="{{ old('email') }}" />
+                                        @error('email')
+                                            <span class="invalid-feedback"><strong>{{ $message }}</strong></span>
+                                        @enderror
+                                    </div>
                                     <div class="form-group">
                                         <label class="small mb-1" for="inputPassword">Password</label>
-                                        <input class="form-control py-4 @error('email') is-invalid @enderror" id="inputPassword" type="password" placeholder="Isi password" name="password"/>
+                                        <input class="form-control py-4 @error('email') is-invalid @enderror"
+                                            id="inputPassword" type="password" placeholder="Isi password"
+                                            name="password" />
                                         @error('password')
-                                            <span class="invalid-feedback"><strong>{{$message}}</strong></span>
-                                        @enderror  
+                                            <span class="invalid-feedback"><strong>{{ $message }}</strong></span>
+                                        @enderror
                                     </div>
                                     <div class="form-group d-flex align-items-center justify-content-between mt-4 mb-0">
-                                    <button type="submit" class="btn btn-primary">{{__('login')}}</button>
+                                        <button type="submit" class="btn btn-primary">{{ __('Login') }}</button>
                                     </div>
                                 </form>
                             </div>
@@ -53,4 +85,5 @@
         </main>
     </div>
 </body>
+
 </html>
