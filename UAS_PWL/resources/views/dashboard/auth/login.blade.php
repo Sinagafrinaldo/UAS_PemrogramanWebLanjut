164cf440aef511f94c527510155594c96d759a1b
Frinaldo Sinaga <84637791+Sinagafrinaldo@users.noreply.github.com>
2022-05-06 16:56:45 +0700

    Update login.blade.php

--- UAS_PWL/resources/views/dashboard/auth/login.blade.php
+++ UAS_PWL/resources/views/dashboard/auth/login.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="shortcut icon" href="/assets/images/icon.svg" type="image/x-icon">
     <title>Login Admin || LiterasiKUY</title>
     <link rel="stylesheet" href="{{ asset('vendor/css/styles.css') }}">
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
