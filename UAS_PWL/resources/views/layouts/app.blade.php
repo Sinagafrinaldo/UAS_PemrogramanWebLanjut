c9e9b7daf10c242f3d1ded4cb84c77a5f46d769c
Frinaldo Sinaga <84637791+Sinagafrinaldo@users.noreply.github.com>
2022-05-06 16:31:03 +0700

    Update footer

--- UAS_PWL/resources/views/layouts/app.blade.php
+++ UAS_PWL/resources/views/layouts/app.blade.php
@@ -24,7 +24,8 @@
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,700;1,400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,700;1,400&display=swap"
+        rel="stylesheet">
     <!-- Styles -->
     <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
 </head>
@@ -38,32 +39,24 @@
         <footer style=" background:#1F76C6" class="text-center text-white">
             <!-- Grid container -->
             <div class="container p-4 pb-0">
-                <!-- Section: Social media -->
+
                 <section class="mb-4">
-                    <!-- Facebook -->
-                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
-                            class="fab fa-facebook-f"></i></a>
 
-                    <!-- Twitter -->
-                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
-                            class="fab fa-twitter"></i></a>
+                    <i>Joy Ravelo Tarigan - 119140037 || </i>
 
-                    <!-- Instagram -->
-                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
-                            class="fab fa-instagram"></i></a>
+                    <i>Frinaldo Sinaga - 119140064</i><br>
+                    <i>Dodi Devrian Andrianto - 119140023 || </i>
+                    <i>Hendamia Yohana Sembiring - 119140178 || </i>
+                    <i>Fahriza Yusefa - 119140005 </i>
 
-                    <!-- Github -->
-                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
-                            class="fab fa-github"></i></a>
+
                 </section>
-                <!-- Section: Social media -->
-            </div>
-            <!-- Grid container -->
 
-            <!-- Copyright -->
+            </div>
+
             <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-                © 2020 Copyright by
-                <a class="text-white" href="#">PWL Project 2022</a>
+                Copyright © 2022 by
+                <a class="text-white" href="#">PWL Project </a>
             </div>
             <!-- Copyright -->
         </footer>
